5d2bad0387958003f340a688bd7ab195ac33dfba
DANANG-PC <danangistu@live.com>
2017-05-01 23:46:57 +0700

    Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Setting;
+use App\Models\Kesehatan;
+use App\Models\Pendidikan;
+use App\Models\Bencana;
+use App\Models\Komunikasi;
+use App\Models\HariBesar;
+use App\Models\Kegiatan;
+use App\Models\Modal;
+use App\Models\Ketrampilan;
+use App\Models\Pemasaran;
+use App\Models\Riset;
+use App\Models\Regulasi;
 
 class DashboardController extends Controller
 {
@@ -16,6 +27,10 @@ class DashboardController extends Controller
     {
         $this->middleware('auth');
         $this->view = 'dashboard.index';
+        $this->count_pelayanan = Kesehatan::count()+Pendidikan::count()+Bencana::count();
+        $this->count_hubungan  = Komunikasi::count()+HariBesar::count()+Kegiatan::count();
+        $this->count_pemberdayaan = Modal::count()+Ketrampilan::count()+Pemasaran::count()+Riset::count();
+        $this->count_regulasi  = Regulasi::count();
     }
 
     /**
@@ -27,6 +42,11 @@ class DashboardController extends Controller
     {
         return view($this->view,[
             'setting' => Setting::firstOrFail(),
+            'count_pelayanan' => $this->count_pelayanan,
+            'count_hubungan' => $this->count_hubungan,
+            'count_pemberdayaan' => $this->count_pemberdayaan,
+            'count_pemberdayaan' => $this->count_pemberdayaan,
+            'count_regulasi' => $this->count_regulasi,
         ]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -26,19 +26,63 @@
     <!--===================================================-->
     <div id="page-content">
         <!-- <div class="col-lg-12"> -->
-			<!--Profile Heading-->
-			<!--===================================================-->
-			<div class="panel">
-				<div class="panel-bg-cover">
-					<img class="img-responsive" src="{{ asset('contents/'.$setting->dashboard_banner) }}" alt="Image">
-				</div>
-				<div class="panel-body">
+            <!--Profile Heading-->
+            <!--===================================================-->
+            <div class="panel">
+                <div class="panel-bg-cover">
+                    <img class="img-responsive" src="{{ asset('contents/'.$setting->dashboard_banner) }}" alt="Image">
+                </div>
+                <div class="panel-body">
                     <h4>Selamat Datang {{ auth()->user()->name }}</h4>
-					<h2>{{ $setting->dashboard_title }} </h2>
+                    <h2>{{ $setting->dashboard_title }} </h2>
                     {!! $setting->dashboard_description !!}
-				</div>
-			</div>
-		<!-- </div> -->
+                </div>
+            </div>
+        <!-- </div> -->
+        <div class="panel">
+            <div class="panel-body">
+                <div class="col-md-6 col-lg-3">
+                    <div class="panel panel-info panel-colorful">
+                        <div class="panel-body text-center">
+                            <p class="text-uppercase mar-btm text-bg">Pelayanan Masyarakat</p>
+                            <i class="fa fa-life-buoy fa-5x"></i>
+                            <hr>
+                            <p class="h2 text-thin">{{ $count_pelayanan }} DATA</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="panel panel-mint panel-colorful">
+                        <div class="panel-body text-center">
+                            <p class="text-uppercase mar-btm text-bg">Pembinaan Hubungan</p>
+                            <i class="fa fa-link fa-5x"></i>
+                            <hr>
+                            <p class="h2 text-thin">{{ $count_hubungan }} DATA</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="panel panel-warning panel-colorful">
+                        <div class="panel-body text-center">
+                            <p class="text-uppercase mar-btm text-bg">Pemberdayaan</p>
+                            <i class="fa fa-asterisk fa-5x"></i>
+                            <hr>
+                            <p class="h2 text-thin">{{ $count_pemberdayaan }} DATA</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="panel panel-pink panel-colorful">
+                        <div class="panel-body text-center">
+                            <p class="text-uppercase mar-btm text-bg">Regulasi</p>
+                            <i class="fa fa-clipboard fa-5x"></i>
+                            <hr>
+                            <p class="h2 text-thin">{{ $count_regulasi }} DATA</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <!--===================================================-->
     <!--End page content-->
